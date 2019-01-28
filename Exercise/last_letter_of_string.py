5537d8e7404787ec1684dee81d7a0700d61143ec
kooshanfilm <kooshanfilm@hotmail.com>
2019-01-27 19:10:36 -0800

    More Python exercise.

--- Exercise/last_letter_of_string.py
+++ Exercise/last_letter_of_string.py
@@ -4,13 +4,10 @@
 
 # koushan  return  n
 
-
-
 def last_leeter(str):
 
     string_leng = len(str)
     print str[string_leng-1]
-
 
 
 last_leeter("koushan")
---
+++ Exercise/playground.py
@@ -0,0 +1,5 @@
+names = [1,2,3,4,5,3,3,3]
+names = set(names)
+names = list(names)
+
+print names
