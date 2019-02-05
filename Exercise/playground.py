ffb60ac3b17422ccf524e03af285c583de81c28d
kooshanfilm <kooshanfilm@hotmail.com>
2019-02-04 17:35:16 -0800

    Clean up and fix few bugs

--- Exercise/playground.py
+++ Exercise/playground.py
@@ -1,2 +1,12 @@
-def multiply(a, b):
-   a * b
+# Write a Python program to calculate the sum of a list of numbers
+
+
+def sum_list(lis):
+    sum = 0
+    for i in lis:
+        sum = sum + sum_list(lis)
+
+    print sum
+
+
+sum_list([1,2,3])
