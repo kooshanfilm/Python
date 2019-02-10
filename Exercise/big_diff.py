9b0d469c7f53508f62dc10bee77e95f0e6a01811
kooshanfilm <kooshanfilm@hotmail.com>
2019-02-10 13:28:41 -0800

    clean up and adding a unittest.

--- Exercise/big_diff.py
+++ Exercise/big_diff.py
@@ -8,7 +8,6 @@ import unittest
 # big_diff([7, 2, 10, 9])  8
 # big_diff([2, 10, 7, 2])  8
 
-
 def big_diff(num):
     diff_two = max(num) - min(num)
     return diff_two
@@ -19,8 +18,6 @@ class BigDiff(unittest.TestCase):
 
     def test_big_diff(self):
         self.assertEqual(big_diff([12,3,1,2]),11)
-
-
 
 if __name__ == '__main__':
     unittest.main()
