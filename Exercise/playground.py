44e395357fb91c124a43bb4351aa819a73190e11
kooshanfilm <kooshanfilm@hotmail.com>
2019-02-13 16:36:22 -0800

    clean up

--- Exercise/playground.py
+++ Exercise/playground.py
@@ -1,5 +1,18 @@
 
 
-str = "ABCD"
-print str.index("A")
 
+
+x = 0
+my_list = [1,2,3,4]
+l = len(my_list)
+second ={}
+while x < l:
+    for num in my_list:
+        if num in second:
+            second[my_list[x]] = ""
+        else:
+            second[my_list[x]] = num
+    x+=1
+
+
+print second
