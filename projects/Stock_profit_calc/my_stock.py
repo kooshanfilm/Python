58e1d96ed0f50459b6d5a05d6b73ab0a113e7e86
kooshanfilm <kooshanfilm@hotmail.com>
2019-02-15 13:48:35 -0800

    Added a new project

--- projects/Stock_profit_calc/my_stock.py
+++ projects/Stock_profit_calc/my_stock.py
@@ -1,5 +1,4 @@
 
-
 # About the project:
 # give how much money I have
 # which stock I want to buy
@@ -14,22 +13,17 @@ class MoneyMachine():
         # stock_sym = raw_input("Stock sym or manual enter")
         stock_price = float(raw_input("Enter stock price :"))
 
-        return user_budget,stock_price
+        return user_budget ,stock_price
 
     def stock_calculation(self):
 
         number_shares = int (user_budget / stock_price)
+        new_user_budget = number_shares * stock_price
+        five_percent = (stock_price * 5 ) /100  # calculate five percent of money
+        stock_five_percent = five_percent + stock_price
+        five_percent_profite = ((number_shares * stock_five_percent) - 20.99) - new_user_budget
 
-        five_percent = (MoneyMachine.stock_price * 5)/100 # calculate five percent of money
-        print number_shares
-
-        # print ("Number of shares you can buy:{} ".format(number_shares))
-
-
-
-
-
-
+        print ("5% profit sell at {} to make {}").format(stock_five_percent ,five_percent_profite)
 
 def main():
 
