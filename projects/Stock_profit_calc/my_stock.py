f4a02269fda7aca83f26aa4ecf19deb77882cae7
kooshanfilm <kooshanfilm@hotmail.com>
2019-02-16 11:37:52 -0800

    fixed variable names in the class and clean up.

--- projects/Stock_profit_calc/my_stock.py
+++ projects/Stock_profit_calc/my_stock.py
@@ -1,39 +1,54 @@
 
 # About the project:
 # give how much money I have
-# which stock I want to buy
+# which stock I want to buy.
 
-class MoneyMachine():
-
+class MoneyMachine:
+    user_budget = 0
+    stock_price = 0
+    new_user_budget = 0
     def user_portfolio(self):
-        global user_budget
-        global stock_price
-        user_budget = int (raw_input("How much you want to buy :"))
-        stock_price = float(raw_input("Enter stock price :"))
 
-        return user_budget ,stock_price
+        self.user_budget = int (raw_input("How much you want to buy :"))
+        self.stock_price = float(raw_input("Enter stock price :"))
+
+        return self.user_budget ,self.stock_price
 
     def stock_calculation(self):
+        number_shares = int (self.user_budget / self.stock_price)
+        self.new_user_budget = number_shares * self.stock_price
 
-        number_shares = int (user_budget / stock_price)
-        new_user_budget = number_shares * stock_price
-
-        for percent in range(3,18,3):
-            stock_percentage = (stock_price * percent ) /100  # calculate five percent of money
-            total_price_after_profit = stock_percentage + stock_price
-            total_price = ((number_shares * total_price_after_profit) - 20.99) - new_user_budget
+        for percent in range(4,18,4):
+            stock_percentage = (self.stock_price * percent ) /100  # calculate x percent of money
+            total_price_after_profit = stock_percentage + self.stock_price
+            total_price = ((number_shares * total_price_after_profit) - 20.99) - self.new_user_budget
             print ("{}% <<stop loss at >> {} profit {}").format(percent,total_price_after_profit ,total_price)
 
+
+    def stock_analize(self):
+
+        print self.new_user_budget
+
+
     # start the project
-    def start_cal(self):
+    def start_project(self):
         user_input = "y"
         while user_input == "y":
             self.user_portfolio()
             self.stock_calculation()
+            self.stock_analize()
             user_input = raw_input("Continue < y > < n >")
+
+
+
 def main():
 
     stock = MoneyMachine()
-    stock.start_cal()
+    stock.start_project()
+    stock.stock_analize()
 if __name__ == '__main__':
     main()
+
+
+
+
