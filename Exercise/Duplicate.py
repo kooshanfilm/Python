5fa08d0129558733289042b4d984d1b81e6da47a
kooshanfilm <kooshanfilm@hotmail.com>
2019-02-20 11:16:07 -0800

    Find a duplicate in the list.

--- Exercise/Duplicate.py
+++ Exercise/Duplicate.py
@@ -1,13 +1,30 @@
-# Write a program that accepts a sequence of whitespace separated words
-# as input and prints the words after removing all duplicate words and sorting
-# them alphanumerically.
-# Suppose the following input is supplied to the program:
-# hello world and practice makes perfect and hello world again
-# Then, the output should be:
-# again and hello makes perfect practice world
+import unittest
 
 
-def remove_duplicate(str):
-   my_list = str.split()
+# [1,2,3,4,2,6,3]
+# [1,2,2,3,3,4,6]
 
-remove_duplicate("Today is friday")
+def find_duplicate(my_arr):
+
+    sorted_arr = sorted(my_arr)
+    duplicate_list = []
+    for i in sorted_arr:
+        if i in duplicate_list:
+            sorted_arr.pop()
+        else:
+            duplicate_list.append(i)
+
+    print duplicate_list
+
+
+
+find_duplicate([1, 2, 3, 4, 2, 6, 3])
+
+# class FindMyDuplicate(unittest.TestCase):
+#
+#     def test_find_duplicate(self):
+#         self.assertEqual(find_duplicate([1, 2, 3, 4, 2, 6, 3]),[2,2,6])
+#
+#
+# if __name__ == '__main__':
+#     unittest.main()
