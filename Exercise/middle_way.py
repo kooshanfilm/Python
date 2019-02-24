cb205236402bd285adf708685f770f1d768150b2
kooshanfilm <kooshanfilm@hotmail.com>
2019-02-24 12:02:13 -0800

    Clean up.

--- Exercise/middle_way.py
+++
@@ -1,8 +0,0 @@
-# Given 2 int arrays, a and b, each length 3,
-# return a new array length 2 containing their middle elements.
-
-
-def middle_way(a, b):
-    return [a[1],b[1]]
-
-middle_way([1,2,3],[1,5,6])
