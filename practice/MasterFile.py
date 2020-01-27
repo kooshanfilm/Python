8174669c68a557346eaba5ab4229f7d0d697d2cb
koushan <kooshanfilm@hotmail.com>
2020-01-26 17:02:05 -0800

    for practice only

--- practice/MasterFile.py
+++ practice/MasterFile.py
@@ -1,7 +1,45 @@
 #python esentials:
 
+'''
+
+remove items
 new_colors = ["r","g","b","y"]
 
 print(new_colors)
 new_colors.pop()
 print(new_colors)
+
+new_colors = ["r","g","b","y"]
+del (new_colors[0])
+
+print(new_colors)
+
+
+#Sorting List: Mutable == changeable
+	
+new_numbers= ['4','3','2','1']
+
+# temp sorting 
+print(sorted(new_numbers))
+print(new_numbers)
+
+#printng len of the function
+print(len(new_numbers))
+
+#perminat sorting
+new_numbers.sort()
+print(new_numbers)
+'''
+
+#Tooples: Unmutable == not change able 
+
+grades = [1,2,3,4,5,6]
+scores = (2,3,4,5,6)
+
+print(type(grades),grades)
+print(type(scores),scores)
+
+scores = (20,3,4,5,6)
+print(scores)
+
+
