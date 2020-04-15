130c58ff7baa907ad27bb80b3928b39fcc51da6e
koushan <kooshanfilm@hotmail.com>
2020-04-14 21:16:57 -0700

    code challenge 1

---
+++ Python_Code_Challenge/P1.py
@@ -0,0 +1,25 @@
+import random
+
+
+print ("welcome to guess number")
+name = raw_input(" Tell me your name: ")
+print("hello " + name)
+
+number = random.randint(1,20)
+for guess_num in range(1,6):
+    guess = int(raw_input("Take a guess: "))
+    if guess < number:
+        print("Your number is lower than our number")
+    elif guess > number:
+        print("Your number is higher than our number")
+    else:
+        break
+
+
+if guess == number:
+    print ("Bingooo " + str(guess_num))
+else:
+    print ("You lost our number was" + number)
+
+
+
