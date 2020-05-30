4ed09d7ee595205ca9a766291037d2433d6a4f6e
koushan <kooshanfilm@hotmail.com>
2020-05-30 11:45:23 -0700

    final version

--- Web_Automation/web_testing/Python_UI_Automation_Template/POM/testAll.py
+++ Web_Automation/web_testing/Python_UI_Automation_Template/POM/testAll.py
@@ -9,23 +9,25 @@ class TestPyOrgBase(unittest.TestCase):
     """
     TBD
     """
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         chrome_options = webdriver.ChromeOptions()
-        # chrome_options.add_argument('headless')
+        chrome_options.add_argument('headless')
         chrome_options.add_argument('window-size=1920x1080')
-        self.driver = webdriver.Chrome(options=chrome_options)
+        cls.driver = webdriver.Chrome(options=chrome_options)
 
-    def tearDown(self):
-        self.driver.close()
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
 
 class TestHome(TestPyOrgBase):
     """
     TBD
     """
     def setUp(self):
-        super().setUp()
-        self.home = HomePage(self.driver)
+        self.home = HomePage(TestHome.driver)
 
+    @unittest.skip('demonstrating skipping the test')
     def test_TC001_py3_doc_button(self):
         self.home.hover_to(CommonPageLocators.DOC)
         self.home.assert_elem_text(CommonPageLocators.PY3_DOC_BUTTON, 'Python 3.x Docs')
@@ -36,16 +38,30 @@ class TestHome(TestPyOrgBase):
         self.home.search_for('blahblah')
         self.home.assert_elem_text(CommonPageLocators.SEARCH_RESULT_LIST, 'No results found.')
 
+    # @unittest.skip
+    def test_TC004_assert_title(self):
+        self.assertEqual(self.home.driver.title, 'Welcome to Python.org')
+
+    def test_TC006_main_menu(self):
+        mm = ['about', 'downloads', 'documentation', 'community', 'success-stories', 'news', 'events', 'DUMMY']
+        for mmenu_item in mm:
+            with self.subTest("asserting menu item: {}".format(mmenu_item)):
+                assert self.home.driver.find_element_by_id(mmenu_item)
+
 class TestAbout(TestPyOrgBase):
     """
     TBD
     """
     def setUp(self):
-        super().setUp()
-        self.about = AboutPage(self.driver)
+        # super().setUpClass()
+        self.about = AboutPage(TestAbout.driver)
 
     def test_TC003_verify_upcoming_events_section_present(self):
         self.about.assert_elem_text(AboutPageLocators.UPCOMING_EVENTS, 'Upcoming Events')
+
+    def test_TC005_assert_url(self):
+        # self.assertTrue('about___' in self.about.driver.current_url)
+        assert 'about' in self.about.driver.current_url
 
 if __name__ == '__main__':
     unittest.main()
